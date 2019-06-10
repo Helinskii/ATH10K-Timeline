f4861e7046227c15f7d25074c0bdeeef617d4b00
Shubham Saha <shubhamsaha@gmail.com>
2019-06-10 22:52:30 +0530

    flag decoding integrated in to original 'parsing' code

--- test/parsing_new.py
+++ test/parsing_new.py
@@ -62,6 +62,7 @@ def parse():
                 pc = line.find('peer create')
                 dasc = line.find('disassociated')
                 pd = line.find('peer delete')
+                flag = line.find('flags')
                 time = line[2:9]
 
                 if ma > 0 and m > 0 and pc > 0:
@@ -84,11 +85,21 @@ def parse():
 
                     new_file.close()
 
-                if ma > 0 and vht > 0:
+                if ma > 0 and vht > 0 and flag > 0:
+                    flag_end = line.find('peer_bw_rxnss_override')
+                    flag = int(line[flag + 6:flag_end - 1], 16)
+
+                    flag_list = flag_decode(flag)
+                    
                     sta_file.write('At time: ' + time + 's from startup\n')
                     sta_file.write('Channel with Station ' + station + ' has Very High Transmission Capabilites\n')
                     sta_file.write('Maximum Length of A-MPDU: ' + line[vht + 36: vht + 42] + ' bytes\n\n')
+                    sta_file.write('Following are the flags enabled for this client:\n\n')
 
+                    for f in flag_list:
+                        sta_file.write(f + '\n')
+                    sta_file.write('\n\n')
+                    
                 if ma > 0 and phm > 0:
                     sta_file.write('At time: ' + time + 's from startup\n')
                     sta_file.write('The mode of the connection is: 802.' + line[phm + 8:phm + 12] + '\n')
@@ -114,6 +125,52 @@ def parse():
         new_file.close()
 
 
+def flag_decode(flag):
+    wmi_peer_flags = {'WMI_PEER_AUTH': 0x00000001,
+                      'WMI_PEER_QOS': 0x00000002,
+                      'WMI_PEER_NEED_PTK_4_WAY': 0x00000004,
+                      'WMI_PEER_NEED_GTK_2_WAY': 0x00000010,
+                      'WMI_PEER_APSD': 0x00000800,
+                      'WMI_PEER_HT': 0x00001000,
+                      'WMI_PEER_40MHZ': 0x00002000,
+                      'WMI_PEER_STBC': 0x00008000,
+                      'WMI_PEER_LDPC': 0x00010000,
+                      'WMI_PEER_DYN_MIMOPS': 0x00020000,
+                      'WMI_PEER_STATIC_MIMOPS': 0x00040000,
+                      'WMI_PEER_SPATIAL_MUX': 0x00200000,
+                      'WMI_PEER_VHT': 0x02000000,
+                      'WMI_PEER_80MHZ': 0x04000000,
+                      'WMI_PEER_VHT_2G': 0x08000000,
+                      'WMI_PEER_PMF': 0x10000000,
+                      'WMI_PEER_160MHZ': 0x20000000
+                      }
+
+    flag_len = len(str(bin(flag)))
+    flag_len -= 2
+
+    flag_list = []
+    
+    for i in range (0, flag_len):
+        flag_value = flag & (1 << i)
+        if flag_value > 0:
+            flag_list.append(flag_value)
+
+    final_flag_list = get_flags(wmi_peer_flags, flag_list)
+            
+    return final_flag_list
+                      
+
+def get_flags(peer_flags_dict, flag_list):
+    list_of_flags = []
+    peer_flag_items = peer_flags_dict.items()
+
+    for item in peer_flag_items:
+        if item[1] in flag_list:
+            list_of_flags.append(item[0])
+
+    return list_of_flags
+
+
+        
 if __name__ == '__main__':
     parse()
-    display_sta()

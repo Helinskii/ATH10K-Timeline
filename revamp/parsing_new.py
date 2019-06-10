70e883df37100db1e416c22972cf19770c8cc3fa
Shubham Saha <shubhamsaha@gmail.com>
2019-06-10 22:32:51 +0530

    decoded wmi peer flags

--- revamp/parsing_new.py
+++ revamp/parsing_new.py
@@ -42,7 +42,7 @@ def parse():
         if match[0] not in macads:
             macads.append(match[0])
 
-    print(macads)
+#    print(macads)
     error_lines = []
     mac_lines = []
     wmi_lines = []
@@ -61,6 +61,60 @@ def parse():
         else:
             misc_lines.append(line)
 
+    for line in mac_lines:
+        flag_loc = line.find('flags')
+        if flag_loc > 0:
+            flag_end = line.find('peer_bw_rxnss_override')
+            flag = int(line[flag_loc + 6:flag_end - 1], 16)
+
+            flag_list = flag_decode(flag)
+            print(flag_list)
+
+            
+def flag_decode(flag):
+    wmi_peer_flags = {'WMI_PEER_AUTH': 0x00000001,
+                      'WMI_PEER_QOS': 0x00000002,
+                      'WMI_PEER_NEED_PTK_4_WAY': 0x00000004,
+                      'WMI_PEER_NEED_GTK_2_WAY': 0x00000010,
+                      'WMI_PEER_APSD': 0x00000800,
+                      'WMI_PEER_HT': 0x00001000,
+                      'WMI_PEER_40MHZ': 0x00002000,
+                      'WMI_PEER_STBC': 0x00008000,
+                      'WMI_PEER_LDPC': 0x00010000,
+                      'WMI_PEER_DYN_MIMOPS': 0x00020000,
+                      'WMI_PEER_STATIC_MIMOPS': 0x00040000,
+                      'WMI_PEER_SPATIAL_MUX': 0x00200000,
+                      'WMI_PEER_VHT': 0x02000000,
+                      'WMI_PEER_80MHZ': 0x04000000,
+                      'WMI_PEER_VHT_2G': 0x08000000,
+                      'WMI_PEER_PMF': 0x10000000,
+                      'WMI_PEER_160MHZ': 0x20000000
+                      }
+
+    flag_len = len(str(bin(flag)))
+    flag_len -= 2
+
+    flag_list = []
+    
+    for i in range (0, flag_len):
+        flag_value = flag & (1 << i)
+        if flag_value > 0:
+            flag_list.append(flag_value)
+
+    final_flag_list = get_flags(wmi_peer_flags, flag_list)
+            
+    return final_flag_list
+                      
+
+def get_flags(peer_flags_dict, flag_list):
+    list_of_flags = []
+    peer_flag_items = peer_flags_dict.items()
+
+    for item in peer_flag_items:
+        if item[1] in flag_list:
+            list_of_flags.append(item[0])
+
+    return list_of_flags
 
 if __name__ == "__main__":
     parse()
